0c001a0819ca44880f07ac82cd2af92961aecacb
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-12-01 09:54:00 +0100

    day01 solution

---
+++ 2019/day01.py
@@ -0,0 +1,40 @@
+"""
+https://adventofcode.com/2019/day/1
+"""
+
+def compute_fuel_required(rocket_mass):
+    return int(rocket_mass / 3) -2
+
+assert compute_fuel_required(12) == 2
+assert compute_fuel_required(14) == 2
+assert compute_fuel_required(1969) == 654
+assert compute_fuel_required(100756) == 33583
+
+with open('day01_input.txt', 'r') as f:
+    modules = [int(line.strip()) for line in f]
+    
+# part 1
+print('------- part 1 -------')
+
+fuel_loads = [compute_fuel_required(mass) for mass in modules]
+print(sum(fuel_loads))
+
+# part 2
+print('------- part 2 -------')
+
+def compute_extended_fuel_required(mass, total=None):
+    if total is None:
+        total = 0
+    fuel = compute_fuel_required(mass)
+    total += fuel
+    if compute_fuel_required(fuel) <= 0:
+        return total
+    else:
+        return compute_extended_fuel_required(fuel, total)
+
+assert compute_extended_fuel_required(14) == 2
+assert compute_extended_fuel_required(1969) == 966
+assert compute_extended_fuel_required(100756) == 50346
+
+loaded_modules = [compute_extended_fuel_required(mass) for mass in modules]
+print(sum(loaded_modules))
