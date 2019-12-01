c9354ae5b46ff7bdc5922627abd5e7673e5fb526
Guillaume Calmettes <guillaume.calmettes@gmail.com>
2019-12-01 13:56:40 +0100

    PEP8 compliant now ;)

--- 2019/day01.py
+++ 2019/day01.py
@@ -2,19 +2,21 @@
 https://adventofcode.com/2019/day/1
 """
 
-def compute_fuel_required(rocket_mass):
-    return int(rocket_mass / 3) -2
+with open('day01_input.txt', 'r') as f:
+    modules = [int(line.strip()) for line in f]
+
+# part 1
+print('------- part 1 -------')
+
+
+def compute_fuel_required(rocket_mass: int) -> int:
+    return int(rocket_mass / 3) - 2
+
 
 assert compute_fuel_required(12) == 2
 assert compute_fuel_required(14) == 2
 assert compute_fuel_required(1969) == 654
 assert compute_fuel_required(100756) == 33583
-
-with open('day01_input.txt', 'r') as f:
-    modules = [int(line.strip()) for line in f]
-    
-# part 1
-print('------- part 1 -------')
 
 fuel_loads = [compute_fuel_required(mass) for mass in modules]
 print(sum(fuel_loads))
@@ -22,15 +24,15 @@ print(sum(fuel_loads))
 # part 2
 print('------- part 2 -------')
 
-def compute_extended_fuel_required(mass, total=None):
-    if total is None:
-        total = 0
+
+def compute_extended_fuel_required(mass: int, total: int=0) -> int:
     fuel = compute_fuel_required(mass)
     total += fuel
     if compute_fuel_required(fuel) <= 0:
         return total
     else:
         return compute_extended_fuel_required(fuel, total)
+
 
 assert compute_extended_fuel_required(14) == 2
 assert compute_extended_fuel_required(1969) == 966
