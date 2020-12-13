b50484c0d5c492d8eb020cef5faad417449db83c
Guillaume CALMETTES <gcalmettes@idmog.com>
2020-12-13 13:58:01 +0100

    [FEAt] animated day11

---
+++ 2020/day11/main_p1.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+	"time"
+)
+
+func main() {
+	data, err := readInput("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	width := len(data[0])
+	height := len(data)
+	g := grid{width, height, map[xy]string{}}
+
+	for y, line := range data {
+		for x, status := range line {
+			g.cells[xy{x, y}] = string(status)
+		}
+	}
+
+	i := 0
+	for {
+		// puth cursor at the top left
+		// see https://student.cs.uwaterloo.ca/~cs452/terminal.html
+		fmt.Print("\033[H\033[2J")
+		fmt.Println(g)
+		before := g.String()
+		g.step()
+		after := g.String()
+		if before == after {
+			break
+		}
+		i++
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
+type xy struct {
+	x, y int
+}
+
+// type grid map[xy]string
+type grid struct {
+	width  int
+	height int
+	cells  map[xy]string
+}
+
+func (g grid) String() string {
+	var n int
+	var out strings.Builder
+	for y := 0; y < g.height; y++ {
+		for x := 0; x < g.width; x++ {
+			status := g.cells[xy{x, y}]
+			if status == "#" {
+				n++
+			}
+			fmt.Fprintf(&out, status)
+		}
+		fmt.Fprint(&out, "\n")
+	}
+	fmt.Fprintf(&out, "Occupied: %d\n", n)
+	return out.String()
+
+}
+
+func (g grid) step() {
+	// we must freeze current state
+	var current = make(map[xy]string)
+	for k, v := range g.cells {
+		current[k] = v
+	}
+	for p, s := range current {
+		if s == "." {
+			// not a seat, pass
+			continue
+		}
+		o := p.occupiedAround(grid{g.width, g.height, current})
+		if (s == "L") && (o == 0) {
+			// becomes occupied
+			g.cells[p] = "#"
+		} else if (s == "#") && (o >= 4) {
+			// becomes empty
+			g.cells[p] = "L"
+		}
+	}
+}
+
+func (pos xy) neighbors(w, h int) []xy {
+	n := make([]xy, 0)
+
+	addLeft := pos.x-1 >= 0
+	addRight := pos.x+1 < w
+	addUpper := pos.y-1 >= 0
+	addLower := pos.y+1 < h
+
+	if addLeft {
+		n = append(n, xy{pos.x - 1, pos.y})
+		if addUpper {
+			n = append(n, xy{pos.x - 1, pos.y - 1})
+		}
+		if addLower {
+			n = append(n, xy{pos.x - 1, pos.y + 1})
+		}
+	}
+	if addRight {
+		n = append(n, xy{pos.x + 1, pos.y})
+		if addUpper {
+			n = append(n, xy{pos.x + 1, pos.y - 1})
+		}
+		if addLower {
+			n = append(n, xy{pos.x + 1, pos.y + 1})
+		}
+	}
+	if addUpper {
+		n = append(n, xy{pos.x, pos.y - 1})
+	}
+	if addLower {
+		n = append(n, xy{pos.x, pos.y + 1})
+	}
+	return n
+}
+
+func (pos xy) occupiedAround(g grid) int {
+
+	neighbors := pos.neighbors(g.width, g.height)
+	c := 0
+	for _, n := range neighbors {
+		if g.cells[n] == "#" {
+			c++
+		}
+	}
+	return c
+}
+
+func readInput(path string) ([]string, error) {
+	d, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not open %s: %v", path, err)
+	}
+
+	lines := strings.Split(string(d), "\n")
+
+	return lines, nil
+}
