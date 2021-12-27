374347c981b603981b7d0b21dad3fc594b126c82
Guillaume Calmettes <gcalmettes@fieldbox.ai>
2021-12-27 10:06:53 +0100

    [ENH] a bit of cleaning

--- 2021/src/day19/main.rs
+++ 2021/src/day19/main.rs
@@ -11,8 +11,8 @@ use std::collections::{HashMap, HashSet};
 // 1- compute all distances of each beacon with the other beacons into a scanner's view
 // 2- initialize grid, using the first scanner as referentiel
 // 3- select scanner having the most common beacons with grid by comparing inter-beacons-distances (see 1)
-// 4- compute center of gravity point for grid and beacons'scanner, and offset both to their COG
-// 5- uses offseted points to calculate rotation, rotate all points, and get translation
+// 4- compute centroid for grid and beacons'scanner, offset both by it to bring them to same origin
+// 5- uses offseted points to calculate rotation, rotate all points, and calculate translation
 // 6- cache translation for part 2 (As they also represent scanner position)
 // 7- use rotation and translation from 5 to get the absolute position of all the beacons of the selected scanner and add them to the grid
 // 8- remove the merged scanner from the list of scanners and repeat from 3 untill there is no more scanner to treat
@@ -20,24 +20,19 @@ use std::collections::{HashMap, HashSet};
 
 type Beacon = (isize, isize, isize);
 
-#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Debug, Eq, PartialEq)]
 struct Scanner {
     id: usize,
-    beacons: Vec<Beacon>
+    beacons: Vec<Beacon>,
+    beacons_interdistances: HashMap<Beacon, HashSet<usize>>,
 }
 
 impl Scanner {
 
-    fn beacons_distance_to_others(&self) -> HashMap<Beacon, HashSet<usize>> {
-        compute_beacons_distance_to_others(&HashSet::from_iter(self.beacons.clone()))
-    }
-
     fn get_scanner_grid_correspondance_map(&self, grid_beacons_inter_distances: &HashMap<Beacon, HashSet<usize>>) -> HashMap<Beacon, Beacon> {
-        let scanner_beacons_inter_distances = self.beacons_distance_to_others();
-
         // we filter by beacon having at least 11 similar distances
         // (group of 12 beacons = 11 neighbors each)
-        iproduct!(grid_beacons_inter_distances.iter(), scanner_beacons_inter_distances.iter())
+        iproduct!(grid_beacons_inter_distances.iter(), self.beacons_interdistances.iter())
             .map(|((gk, gv), (sk, sv))| (*gk, *sk, gv.intersection(sv).count()))
             .filter(|(_, _, count)| *count >= 11)
             .map(|(gk, sk, _)| (sk, gk))
@@ -73,8 +68,8 @@ fn compute_translation_and_rotation(mapping: &HashMap<Beacon, Beacon>) -> (Vec<i
 
     let n = mapping.len() as f32;
 
-    // center of gravity for grid
-    let grid_cog: Vec<f32> = mapping.values()
+    // centroid for grid
+    let grid_centroid: Vec<f32> = mapping.values()
         .fold([0; 3], |acc, (x, y, z)| {
             [acc[0] + x, acc[1] + y, acc[2] + z]
         })
@@ -82,8 +77,8 @@ fn compute_translation_and_rotation(mapping: &HashMap<Beacon, Beacon>) -> (Vec<i
         .map(|v| (*v as f32) / n)
         .collect();
 
-    // center of gravity for sensor
-    let sensor_cog: Vec<f32> = mapping.keys()
+    // centroid for sensor
+    let sensor_centroid: Vec<f32> = mapping.keys()
         .fold([0; 3], |acc, (x, y, z)| {
             [acc[0] + x, acc[1] + y, acc[2] + z]
         })
@@ -91,22 +86,22 @@ fn compute_translation_and_rotation(mapping: &HashMap<Beacon, Beacon>) -> (Vec<i
         .map(|v| (*v as f32) / n)
         .collect();
 
-    // offset a beacon compared to center of gravity
+    // select a beacon from which we will compute rotation and translation
     let sensor_beacon = mapping.keys().last().unwrap();
     let grid_beacon = mapping.get(sensor_beacon).unwrap();
     
     let offsetted_sensor_beacon = [
-        (sensor_beacon.0 as f32 - sensor_cog[0]).round(),
-        (sensor_beacon.1 as f32 - sensor_cog[1]).round(),
-        (sensor_beacon.2 as f32 - sensor_cog[2]).round(),
+        (sensor_beacon.0 as f32 - sensor_centroid[0]).round(),
+        (sensor_beacon.1 as f32 - sensor_centroid[1]).round(),
+        (sensor_beacon.2 as f32 - sensor_centroid[2]).round(),
     ];
     let offsetted_grid_beacon = [
-        (grid_beacon.0 as f32 - grid_cog[0]).round(),
-        (grid_beacon.1 as f32 - grid_cog[1]).round(),
-        (grid_beacon.2 as f32 - grid_cog[2]).round(),
+        (grid_beacon.0 as f32 - grid_centroid[0]).round(),
+        (grid_beacon.1 as f32 - grid_centroid[1]).round(),
+        (grid_beacon.2 as f32 - grid_centroid[2]).round(),
     ];
 
-    // compute rotation by finding common offsetted coordinates
+    // compute rotation by finding common offsetted coordinates components (x, y, z)
     let mut rotation: HashMap<usize, (usize, isize)> = HashMap::new();
     iproduct!(
         offsetted_grid_beacon.iter().enumerate(),
@@ -124,7 +119,7 @@ fn compute_translation_and_rotation(mapping: &HashMap<Beacon, Beacon>) -> (Vec<i
             rotated_sensor_beacon[*axis] = sensor_beacon[rotation.0] * rotation.1;
         });
 
-    // translation
+    // compute translation using difference in distance of same beacon in scanner and grid view
     let grid_beacon = [grid_beacon.0, grid_beacon.1, grid_beacon.2];
     let translation = rotated_sensor_beacon.iter()
         .zip(grid_beacon)
@@ -134,7 +129,7 @@ fn compute_translation_and_rotation(mapping: &HashMap<Beacon, Beacon>) -> (Vec<i
     (translation, rotation)
 }
 
-fn apply_transformation(trans: &Vec<isize>, rot: &HashMap<usize, (usize, isize)>, beacons: &Vec<Beacon>) -> HashSet<Beacon> {
+fn apply_transform(trans: &Vec<isize>, rot: &HashMap<usize, (usize, isize)>, beacons: &Vec<Beacon>) -> HashSet<Beacon> {
     beacons.iter()
         .map(|b| {
             let original = [b.0, b.1, b.2];
@@ -164,11 +159,11 @@ fn part1(input: &str) -> (usize, Vec<Vec<isize>>) {
         let (idx, beacon_mapping) = get_best_candidate_scanner_idx_and_mapping(&scanners, &grid_distances);
         let selected_scanner = &scanners[idx];
 
-        // compute transformation (translation + rotation) of the scanner referentiel into grid referentiel
+        // compute transform (translation + rotation) of the scanner referentiel into grid referentiel
         let (translation, rotation) = compute_translation_and_rotation(&beacon_mapping);
 
         // beacons coordinates in grid referentiel
-        let transformed_beacons = apply_transformation(&translation, &rotation, &selected_scanner.beacons);
+        let transformed_beacons = apply_transform(&translation, &rotation, &selected_scanner.beacons);
 
         grid.extend(transformed_beacons);
         scanners.remove(idx);
@@ -211,7 +206,8 @@ fn parse_input(content: &str) -> Vec<Scanner> {
 
             Scanner{
                 id: scanner_id,
-                beacons
+                beacons: beacons.clone(),
+                beacons_interdistances: compute_beacons_distance_to_others(&HashSet::from_iter(beacons))
             }
         })
     .collect()
