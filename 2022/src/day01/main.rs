886008afdd7905b1469b7732d417891e4439decd
gcalmettes <gcalmettes@idmog.com>
2022-12-01 15:56:18 +0100

    feat: 2022 day01

---
+++ 2022/src/day01/main.rs
@@ -0,0 +1,26 @@
+use itertools::Itertools;
+use std::fs;
+
+fn get_sorted_elves() -> Vec<usize> {
+    let input = fs::read_to_string("./inputs/01.in")
+        .expect("file not found");
+
+    input.split("\n\n")
+        .collect::<Vec<&str>>()
+        .iter()
+        .map(|elf| elf.lines()
+                    .map(|l| l.parse::<usize>().unwrap())
+                    .sum::<usize>()
+                    )
+        .sorted()
+        .rev()
+        .collect::<Vec<usize>>()
+}
+
+fn main() {
+    let elves = get_sorted_elves();
+    let p1 = elves[0];
+    let p2 = &elves[0..3].iter().sum::<usize>();
+    println!("part 1: {:?}", p1);
+    println!("part 2: {:?}", p2);
+}
