94414701f7c07aa441925e4bb9067c4dc8ec9814
Guillaume CALMETTES <gcalmettes@idmog.com>
2022-12-21 08:03:37 +0100

    feat: add old 20 solution for archive

--- 2022/src/bin/20.rs
+++ 2022/src/bin/20.rs
@@ -50,3 +50,78 @@ fn main(input: &str) -> (i64, i64) {
     let p2 = part2(&data);
     (p1, p2)
 }
+
+// =============================================================================================
+// OLD SOLUTION
+// =============================================================================================
+
+// use hashbrown::HashMap;
+// use itertools::Itertools;
+
+// fn order(file: &HashMap<usize, (usize, i64)>) -> Vec<i64> {
+//     let nums = file
+//         .iter()
+//         .sorted_by(|a, b| a.0.cmp(&b.0))
+//         .map(|(_, (_, v))| *v)
+//         .collect();
+//     nums
+// }
+
+// fn decrypt(input: &str, iterations: usize, decryption_key: i64) -> i64 {
+//     let mut file = HashMap::new();
+//     input.lines().enumerate().for_each(|(i, line)| {
+//         let num = line.parse::<i64>().unwrap() * decryption_key;
+//         file.insert(i, (i, num));
+//     });
+
+//     for i in 0..file.len() {
+//         let payload = file[&i];
+//         *file.get_mut(&i).unwrap() = payload;
+//     }
+//     for _ in 0..iterations {
+//         for i in 0..file.len() {
+//             // find current position of num
+//             let (pos, (origin, shift_by)) = file.iter().find(|(_, (idx, _))| idx == &i).unwrap();
+//             let payload = (*origin, *shift_by);
+
+//             let target_pos =
+//                 ((*pos as i64) + shift_by).rem_euclid((file.len() - 1) as i64) as usize;
+
+//             if pos == &target_pos {
+//                 // already is already in place
+//                 continue;
+//             } else if &target_pos > pos {
+//                 // move all numbers in between
+//                 (*pos..target_pos).for_each(|p| {
+//                     *file.get_mut(&p).unwrap() = file[&(p + 1)];
+//                 });
+//                 // then move to target
+//                 *file.get_mut(&target_pos).unwrap() = payload;
+//             } else {
+//                 // move all numbers in between
+//                 let start = target_pos + 1;
+//                 let end = *pos;
+//                 (start..=end).rev().for_each(|p| {
+//                     *file.get_mut(&p).unwrap() = file[&(p - 1)];
+//                 });
+//                 // then move to target
+//                 *file.get_mut(&target_pos).unwrap() = payload;
+//             }
+//             // println!("{:?}", order(&file));
+//         }
+//     }
+//     let nums = order(&file);
+//     let zero_pos = nums.iter().position(|x| x == &(0 as i64)).unwrap();
+//     [1000, 2000, 3000]
+//         .iter()
+//         .map(|i| (i + zero_pos) % file.len())
+//         .map(|i| nums[i])
+//         .sum()
+// }
+
+// #[aoc::main()]
+// fn main(input: &str) -> (i64, i64) {
+//     let p1 = decrypt(input, 1, 1);
+//     let p2 = decrypt(input, 10, 811589153);
+//     (p1, p2)
+// }
