0b6fef15364364863fdc985af8e89c34f2733692
gcalmettes <guillaume.calmettes@gmail.com>
2023-12-20 23:14:04 +0100

    feat: day 20 p1

---
+++ 2023/src/bin/20.rs
@@ -0,0 +1,139 @@
+use std::collections::{HashMap, VecDeque};
+
+#[derive(Debug, PartialEq)]
+enum ModuleType {
+    FlipFlop,
+    Conjunction,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum FlipFlop {
+    Off,
+    On,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Pulse {
+    High,
+    Low,
+}
+
+impl ModuleType {
+    fn from_str(s: &str) -> Option<ModuleType> {
+        match s {
+            "%" => Some(ModuleType::FlipFlop),
+            "&" => Some(ModuleType::Conjunction),
+            _ => None,
+        }
+    }
+}
+
+/// broadcaster targets, modules
+fn parse_modules(input: &str) -> (Vec<&str>, HashMap<&str, (ModuleType, Vec<&str>)>) {
+    input.lines().fold(
+        (vec![], HashMap::new()),
+        |(mut broadcast_targets, mut modules), line| {
+            let (mut name, children) = line.split_once(" -> ").unwrap();
+            let destination = children.split(", ").collect::<Vec<&str>>();
+
+            if let Some(mod_type) = ModuleType::from_str(name.get(0..1).unwrap()) {
+                name = name.get(1..).unwrap();
+                modules.insert(name, (mod_type, destination));
+            } else {
+                broadcast_targets = destination
+            };
+            (broadcast_targets, modules)
+        },
+    )
+}
+
+fn simulate(targets: &Vec<&str>, modules: &HashMap<&str, (ModuleType, Vec<&str>)>) -> usize {
+    // get list of inputs for each conjunction module and initialize them
+    let (mut conj_inputs, mut flipflop_state) = modules.iter().fold(
+        (HashMap::new(), HashMap::new()),
+        |(mut conj, mut flipflop), (name, (mod_type, _))| {
+            match mod_type {
+                &ModuleType::Conjunction => {
+                    conj.insert(name, HashMap::<&str, Pulse>::new());
+                }
+                &ModuleType::FlipFlop => {
+                    flipflop.insert(name, FlipFlop::Off);
+                }
+            }
+            (conj, flipflop)
+        },
+    );
+    for module in modules {
+        let (name, (_mod_type, destinations)) = module;
+        for dest in destinations {
+            if conj_inputs.contains_key(dest) {
+                conj_inputs.get_mut(dest).unwrap().insert(name, Pulse::Low);
+            }
+        }
+    }
+
+    // simulation
+    let mut pulse_low = 0;
+    let mut pulse_high = 0;
+
+    for _ in 0..1000 {
+        // we press the button
+        pulse_low += 1;
+
+        let mut queue =
+            VecDeque::from_iter(targets.iter().map(|name| ("brodcaster", Pulse::Low, *name)));
+
+        while let Some((src, pulse, dest)) = queue.pop_front() {
+            // println!("{:?} {:?} {:?}", src, pulse, dest);
+
+            if pulse == Pulse::High {
+                pulse_high += 1;
+            } else {
+                pulse_low += 1;
+            }
+            if let Some((mod_type, destinations)) = modules.get(dest) {
+                let pulse_to_send = match mod_type {
+                    &ModuleType::Conjunction => {
+                        // update pulse memory for the input
+                        let inputs = conj_inputs.get_mut(&dest).unwrap();
+                        *inputs.get_mut(src).unwrap() = pulse;
+                        match inputs.iter().all(|(_, p)| p == &Pulse::High) {
+                            true => Some(Pulse::Low),
+                            false => Some(Pulse::High),
+                        }
+                    }
+                    &ModuleType::FlipFlop => {
+                        let state = flipflop_state.get_mut(&dest).unwrap();
+                        match (pulse, state.clone()) {
+                            (Pulse::Low, FlipFlop::Off) => {
+                                *state = FlipFlop::On;
+                                Some(Pulse::High)
+                            }
+                            (Pulse::Low, FlipFlop::On) => {
+                                *state = FlipFlop::Off;
+                                Some(Pulse::Low)
+                            }
+                            (Pulse::High, _) => None,
+                        }
+                    }
+                };
+                if let Some(pulse_to_send) = pulse_to_send {
+                    for new_dest in destinations {
+                        queue.push_back((dest, pulse_to_send, new_dest));
+                    }
+                }
+            } else {
+                continue;
+            }
+        }
+    }
+    pulse_low * pulse_high
+}
+
+#[aoc::main()]
+fn main(input: &str) -> (usize, usize) {
+    let (broadcast_targets, modules) = parse_modules(input);
+    let p1 = simulate(&broadcast_targets, &modules);
+    let p2 = 0;
+    (p1, p2)
+}
