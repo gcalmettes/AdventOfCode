2e7424197937994373869e6fe278e4f6a92a91c3
gcalmettes <guillaume.calmettes@gmail.com>
2023-12-27 19:29:09 +0100

    day 20 part 2

--- 2023/src/bin/20.rs
+++ 2023/src/bin/20.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use num::integer::lcm;
 use std::collections::{HashMap, VecDeque};
 
 #[derive(Debug, PartialEq)]
@@ -50,6 +51,7 @@ fn parse_modules(input: &str) -> (Vec<&str>, HashMap<&str, (ModuleType, Vec<&str
 
 fn generate_dot(broadcast_targets: &Vec<&str>, modules: &HashMap<&str, (ModuleType, Vec<&str>)>) {
     let mut dot = String::from("digraph G {\n");
+    dot += &format!("  {{ rank = sink; rx; }};\n");
     dot += &format!("  button -> brodcaster;\n");
     for t in broadcast_targets.iter().sorted() {
         dot += &format!("  brodcaster -> {};\n", t);
@@ -157,11 +159,114 @@ fn simulate(targets: &Vec<&str>, modules: &HashMap<&str, (ModuleType, Vec<&str>)
     pulse_low * pulse_high
 }
 
+fn find_cycles(
+    targets: &Vec<&str>,
+    modules: &HashMap<&str, (ModuleType, Vec<&str>)>,
+) -> Vec<usize> {
+    // get list of inputs for each conjunction module and initialize them
+    let (mut conj_inputs, mut flipflop_state) = modules.iter().fold(
+        (HashMap::new(), HashMap::new()),
+        |(mut conj, mut flipflop), (name, (mod_type, _))| {
+            match mod_type {
+                &ModuleType::Conjunction => {
+                    conj.insert(name, HashMap::<&str, Pulse>::new());
+                }
+                &ModuleType::FlipFlop => {
+                    flipflop.insert(name, FlipFlop::Off);
+                }
+            }
+            (conj, flipflop)
+        },
+    );
+    let mut to_rx = "tbd";
+    for module in modules {
+        let (name, (_mod_type, destinations)) = module;
+        // find input for rx
+        if destinations.contains(&&"rx") {
+            to_rx = name;
+        }
+        for dest in destinations {
+            if conj_inputs.contains_key(dest) {
+                conj_inputs.get_mut(dest).unwrap().insert(name, Pulse::Low);
+            }
+        }
+    }
+
+    let to_be_synchronized = conj_inputs
+        .get(&&to_rx)
+        .unwrap()
+        .iter()
+        .map(|(k, _)| *k)
+        .collect::<Vec<&str>>();
+
+    let mut cycles: HashMap<&str, Vec<usize>> = HashMap::new();
+    let mut cycle = 0;
+
+    while (cycles.len() < to_be_synchronized.len()) | cycles.iter().any(|(_k, v)| v.len() != 2) {
+        cycle += 1;
+
+        let mut queue =
+            VecDeque::from_iter(targets.iter().map(|name| ("brodcaster", Pulse::Low, *name)));
+
+        while let Some((src, pulse, dest)) = queue.pop_front() {
+            if to_be_synchronized.contains(&&src) && pulse == Pulse::High {
+                let module_cycles = cycles.entry(src).or_insert(vec![]);
+                if module_cycles.len() < 2 {
+                    module_cycles.push(cycle)
+                }
+                if cycles.len() == to_be_synchronized.len()
+                    && cycles.iter().all(|(_k, v)| v.len() == 2)
+                {
+                    return cycles.values().map(|v| v[1] - v[0]).collect();
+                }
+            }
+
+            if let Some((mod_type, destinations)) = modules.get(dest) {
+                let pulse_to_send = match mod_type {
+                    &ModuleType::Conjunction => {
+                        // update pulse memory for the input
+                        let inputs = conj_inputs.get_mut(&dest).unwrap();
+                        *inputs.get_mut(src).unwrap() = pulse;
+                        match inputs.iter().all(|(_, p)| p == &Pulse::High) {
+                            true => Some(Pulse::Low),
+                            false => Some(Pulse::High),
+                        }
+                    }
+                    &ModuleType::FlipFlop => {
+                        let state = flipflop_state.get_mut(&dest).unwrap();
+                        match (pulse, state.clone()) {
+                            (Pulse::Low, FlipFlop::Off) => {
+                                *state = FlipFlop::On;
+                                Some(Pulse::High)
+                            }
+                            (Pulse::Low, FlipFlop::On) => {
+                                *state = FlipFlop::Off;
+                                Some(Pulse::Low)
+                            }
+                            (Pulse::High, _) => None,
+                        }
+                    }
+                };
+                if let Some(pulse_to_send) = pulse_to_send {
+                    for new_dest in destinations {
+                        queue.push_back((dest, pulse_to_send, new_dest));
+                    }
+                }
+            } else {
+                continue;
+            }
+        }
+    }
+    unreachable!()
+}
+
 #[aoc::main()]
 fn main(input: &str) -> (usize, usize) {
     let (broadcast_targets, modules) = parse_modules(input);
     let p1 = simulate(&broadcast_targets, &modules);
-    generate_dot(&broadcast_targets, &modules);
-    let p2 = 0;
+    // generate graph to understand what's going on
+    // generate_dot(&broadcast_targets, &modules);
+    let cycles = find_cycles(&broadcast_targets, &modules);
+    let p2 = cycles.into_iter().fold(1, |acc, c| lcm(c, acc));
     (p1, p2)
 }
